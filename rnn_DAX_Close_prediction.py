650da9068cecc5b344dc92cc44caf399df7e366d
Peter Spisak <spispes@gmail.com>
2019-02-26 17:40:47 +0100

    Documentation updated

--- rnn_DAX_Close_prediction.py
+++ rnn_DAX_Close_prediction.py
@@ -17,7 +17,7 @@ training_set_close = getClose(dataset_train)
 # Feature Scaling
 from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
-training_set_open_scaled = sc.fit_transform(training_set_close.reshape(-1,1))
+training_set_open_scaled = sc.fit_transform(training_set_open.reshape(-1,1))
 training_set_close_scaled = sc.fit_transform(training_set_close.reshape(-1,1))
 
 """
@@ -29,16 +29,18 @@ training_set_close_scaled = sc.fit_transform(training_set_close.reshape(-1,1))
 X_train_close = []
 y_train_close = []
 for i in range(60, 2280):
-    X_train_close.append(training_set_open_scaled[i-60:i, 0])
+    X_train_close.append(training_set_close_scaled[i-60:i, 0])
     y_train_close.append(training_set_close_scaled[i, 0])
 X_train_close, y_train_close = np.array(X_train_close), np.array(y_train_close)
 
-# Reshaping
+""" 
+Reshaping adding a secon dimention to the network
+Impup is X_train_close the prepared matrix and the connection is to
+number of data sets = X_train_close.shape[0]
+number of colums = X_train_close.shape[1] //60 in this case
+"""
 X_train_close = np.reshape(X_train_close, (X_train_close.shape[0], X_train_close.shape[1], 1))
 
-# Building the RNN for open
-
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
@@ -71,7 +73,6 @@ regressor_close.compile(optimizer = 'adam', loss = 'mean_squared_error')
 
 # Fitting the RNN to the Training set
 regressor_close.fit(X_train_close, y_train_close, epochs = 100, batch_size = 32)
-regressor_close.
 
 """
 
@@ -81,17 +82,32 @@ regressor_close.
 
 """
 # Getting the real stock price of 2019
-dataset_test = prepareInput('GDAXI_test.csv')
-real_stock_price_close = getClose(dataset_test)
+dataset_predict = prepareInput('GDAXI_test.csv')
+real_stock_price_close = getClose(dataset_predict)
 
 import pandas as pd
 # Getting the predicted stock price of for opening
-dataset_total_close = pd.concat((dataset_train['Close'], dataset_test['Close']), axis = 0)
-inputs = dataset_total_close[len(dataset_total_close) - len(dataset_test) - 60:].values
+dataset_total_close = pd.concat((dataset_train['Close'], dataset_predict['Close']), axis = 0)
+"""
+input are the days to be predicted + 60 days from the past
+e.g. dataset_predict contains 39 items --> input = 39 + 60
+The last item in the list is the prediction for the next day
+The last step in input preparation is to normalize the input range 0:1
+"""
+inputs = dataset_total_close[len(dataset_total_close) - len(dataset_predict) - 60:].values
 inputs = inputs.reshape(-1,1)
 inputs = sc.transform(inputs)
-
-maxLen = len(inputs)
+"""
+Prepare a prediction set where for each entry in dataset_predict
+the last 60 days are added e.g.
+[close day 0, close day -1, close day -2, ..., close day -60 ]
+[close day 1, close day -1, close day -2, ..., close day -59 ]
+[close day 2, close day -1, close day -2, ..., close day -58 ]
+...
+[close day max, close day -1, close day -2, ..., close day max - 60 ]
+"""
+# len + 1 is the last predicted day but there are no close values for this 
+maxLen = len(inputs) +1
 X_predict_close = []
 for i in range(60, maxLen):
     X_predict_close.append(inputs[i-60:i, 0])
