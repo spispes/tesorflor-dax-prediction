b2905a4835d7bd1bce5e435ea1ceae5926035bf1
Peter Spisak <peter@spisak.de>
2019-04-18 16:45:31 +0200

    FSTE added increase the epochs for FSTE to 300

---
+++ predict_FTSE.py
@@ -0,0 +1,62 @@
+# Data Preprocessing
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+from prepare_input import prepareInput
+from prepare_input import getClose, getOpen
+from loadRegresor import load_regressor
+
+
+dataset = prepareInput('./daten/FTSE_lerndaten.csv')
+dataset_predict = prepareInput('./daten/FTSE_to_be_predicted.csv')
+dataset_total_close = pd.concat((dataset['Close'], dataset_predict['Close']), axis = 0)
+
+regressor_close = load_regressor('dax_2_regressor')
+
+#sc = MinMaxScaler(feature_range = (0, 1))
+sc = MinMaxScaler(feature_range = (0, 1))
+training_set_open = getOpen(dataset)
+training_set_open = sc.fit_transform(training_set_open.reshape(-1,1))
+
+"""
+input are the days to be predicted + 60 days from the past
+e.g. dataset_predict contains 39 items --> input = 39 + 60
+The last item in the list is the prediction for the next day
+The last step in input preparation is to normalize the input range 0:1
+"""
+timestamp = 60
+
+inputs = dataset_total_close[len(dataset_total_close) - len(dataset_predict) - timestamp:].values
+inputs = sc.transform(inputs.reshape(-1,1))
+#inputs = sc.transform(inputs)
+"""
+Prepare a prediction set where for each entry in dataset_predict
+the last 60 days are added e.g.
+[close day 0, close day -1, close day -2, ..., close day -60 ]
+[close day 1, close day -1, close day -2, ..., close day -59 ]
+[close day 2, close day -1, close day -2, ..., close day -58 ]
+...
+[close day max, close day -1, close day -2, ..., close day max - 60 ]
+"""
+# len + 1 is the last predicted day but there are no close values for this 
+maxLen = len(inputs) +1
+X_predict_close = []
+for i in range(timestamp, maxLen):
+    X_predict_close.append(inputs[i-timestamp:i, 0])
+X_predict_close = np.array(X_predict_close)
+X_predict_close = np.reshape(X_predict_close, (X_predict_close.shape[0], X_predict_close.shape[1], 1))
+
+predicted_stock_price_close = regressor_close.predict(X_predict_close)
+predicted_stock_price_close = sc.inverse_transform(predicted_stock_price_close)
+
+real_stock_price_close = getClose(dataset_predict)
+# Visualising the results
+plt.plot(real_stock_price_close, color = 'red', label = 'Real FTSE Stock Price')
+plt.plot(predicted_stock_price_close, color = 'blue', label = 'Predicted FTSE Stock Close Price')
+plt.grid(b=None, which='major', axis='both')
+plt.title('FTSE Stock Close Price Prediction')
+plt.legend()
+plt.show()
--- train.py
+++ train.py
@@ -28,7 +28,8 @@ def train(filename, inputColumn, outputColumn):
     regressor.add(Dense(units = 1))
     # test_2019-04-18_07-14-07_dji_regressor schlecht regressor.compile(optimizer = 'rmsprop', loss = 'mean_squared_logarithmic_error')
     # test_2019-04-18_07-51-17_dji_regressor schlecht regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-    regressor.fit(model, reference, epochs = 120, batch_size = 32)
+    regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
+    regressor.fit(model, reference, epochs = 300, batch_size = 32)
     persistModel(regressor)
     
     
@@ -38,9 +39,9 @@ def persistModel(regressor):
     regressor_json = regressor.to_json()
     ts = time.time()
     st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d_%H-%M-%S')
-    with open("regressors/test_"+st+"_dji_regressor.json", "w") as json_file:
+    with open("regressors/test_"+st+"_ftse_regressor.json", "w") as json_file:
         json_file.write(regressor_json)
-    regressor.save_weights("regressors/test_"+st+"_dji_regressor.h5")
+    regressor.save_weights("regressors/test_"+st+"_ftse_regressor.h5")
 
 
 def createModel(trainSet, inputColumn):
@@ -72,4 +73,4 @@ def scale(unscaledSet):
     return scaledSet
 
 
-train('./daten/DOW_JONES_lerndaten.csv', 'Close', 'Close')
+train('./daten/FTSE_lerndaten.csv', 'Close', 'Close')
